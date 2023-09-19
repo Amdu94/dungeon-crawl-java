42e0064ccae9d25ea6fcad3439d8deab7241b129
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2023-09-19 21:01:02 +0200

    add item field, item getter and setter, hasItem and removeItem methods

--- src/main/java/com/codecool/dungeoncrawl/data/Cell.java
+++ src/main/java/com/codecool/dungeoncrawl/data/Cell.java
@@ -1,18 +1,21 @@
 package com.codecool.dungeoncrawl.data;
 
 import com.codecool.dungeoncrawl.data.actors.Actor;
+import com.codecool.dungeoncrawl.data.items.Item;
 
 public class Cell implements Drawable {
     private CellType type;
     private Actor actor;
     private GameMap gameMap;
     private int x, y;
+    private Item item;
 
     public Cell(GameMap gameMap, int x, int y, CellType type) {
         this.gameMap = gameMap;
         this.x = x;
         this.y = y;
         this.type = type;
+        this.item = null;
     }
 
     public CellType getType() {
@@ -46,5 +49,21 @@ public class Cell implements Drawable {
 
     public int getY() {
         return y;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public boolean hasItem() {
+        return item != null;
+    }
+
+    public void removeItem() {
+        item = null;
     }
 }
