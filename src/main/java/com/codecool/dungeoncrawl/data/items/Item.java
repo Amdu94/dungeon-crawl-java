3d79bae5481654cd875f5836ba363a3b51c2fd83
Lajos Savoly <125366427+SavolyLali@users.noreply.github.com>
2023-09-19 17:38:52 +0200

    start implementing Item class

---
+++ src/main/java/com/codecool/dungeoncrawl/data/items/Item.java
@@ -0,0 +1,14 @@
+package com.codecool.dungeoncrawl.data.items;
+
+import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.Drawable;
+
+public abstract class Item implements Drawable {
+    private Cell cell;
+    private final String name;
+
+    public Item(Cell cell, String name) {
+        this.cell = cell;
+        this.name = name;
+    }
+}
